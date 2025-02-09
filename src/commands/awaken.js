a36cac55291f8627ba3a3477828159b0ac36c94c
404MW <404mwaqas@gmail.com>
2025-02-09 08:14:38 -0800

    updated /awaken

--- src/commands/awaken.js
+++ src/commands/awaken.js
@@ -14,104 +14,140 @@ module.exports = {
     ),
 
   async execute(interaction) {
-    const allowedChannelId = "1328069031118377103";
-    if (interaction.channelId !== allowedChannelId) {
-      return interaction.reply({
-        content: "This command can only be used in #bot-spam channel.",
-        flags: MessageFlags.Ephemeral,
-      });
-    }
+    // const allowedChannelId = "1328069031118377103";
+    // if (interaction.channelId !== allowedChannelId) {
+    //   return interaction.reply({
+    //     content: "This command can only be used in #bot-spam channel.",
+    //     flags: MessageFlags.Ephemeral,
+    //   });
+    // }
 
     const pool = [
       {
         answer: "E-",
         probability: 0.043,
         emoji: `<:awakenem:1328076005197348984>`,
+        retire: 10,
+        points: 3
       },
       {
         answer: "E",
         probability: 0.198,
         emoji: `<:awakene:1328071268146085969>`,
+        retire: 15,
+        points: 4
       },
       {
         answer: "E+",
         probability: 0.288,
         emoji: `<:awakenep:1328076026110148739>`,
+        retire: 20,
+        points: 5
       },
       {
         answer: "D-",
         probability: 0.2,
         emoji: `<:awakendm:1328075963699040389>`,
+        retire: 30,
+        points: 6
       },
       {
         answer: "D",
         probability: 0.092,
         emoji: `<:awakend:1328071247384416286>`,
+        retire: 50,
+        points: 7
       },
       {
         answer: "D+",
         probability: 0.048,
         emoji: `<:awakendp:1328075988877443073>`,
+        retire: 70,
+        points: 8
       },
       {
         answer: "C-",
         probability: 0.044,
         emoji: "<:awakencm:1328075931754954772>",
+        retire: 100,
+        points: 9
       },
       {
         answer: "C",
         probability: 0.043,
         emoji: "<:awakenc:1328071228300197980>",
+        retire: 150,
+        points: 10
       },
       {
         answer: "C+",
         probability: 0.0213,
         emoji: "<:awakencp:1328075947437592676>",
+        retire: 200,
+        points: 11
       },
       {
         answer: "B-",
         probability: 0.0162,
         emoji: "<:awakenbm:1328076518412521564>",
+        retire: 300,
+        points: 12
       },
       {
         answer: "B",
         probability: 0.0055,
         emoji: "<:awakenb:1328071210860286124>",
+        retire: 600,
+        points: 13
       },
       {
         answer: "B+",
         probability: 0.000745,
         emoji: "<:awakenbp:1328076535055384757>",
+        retire: 1800,
+        points: 14
       },
       {
         answer: "A-",
         probability: 0.00015,
         emoji: "<:awakenam:1328076470895116319>",
+        retire: 8000,
+        points: 15
       },
       {
         answer: "A",
         probability: 0.000065,
         emoji: "<:awakena:1328071192753602600>",
+        retire: 15000,
+        points: 16
       },
       {
         answer: "A+",
         probability: 0.000025,
         emoji: "<:awakenap:1328076502218313799>",
+        retire: 25000,
+        points: 17
       },
       {
         answer: "S",
-        probability: 0.000005,
+        probability: 0.000006,
         emoji: "<:awakens:1328071289763659949>",
+        retire: 50000,
+        points: 18
       },
       {
         answer: "SS",
         probability: 0.000005,
         emoji: "<:awakenss:1329829671113855087>",
+        retire: 100000,
+        points: 19
       },
       {
         answer: "SSS",
-        probability: 0.000005,
+        probability: 0.000004,
         emoji: "<:awakensss:1329829693968613428>",
+        retire: 200000,
+        points: 20
       },
     ];
 
@@ -151,14 +187,20 @@ module.exports = {
     const results = runMultipleSelections(pool, iterations);
 
     let reply = `You awakened **${iterations}** times:\n`;
+    let totalRetire = 0
+    let totalPoints = 0
 
     for (const [answer, count] of Object.entries(results)) {
       if (count > 0) {
         const poolItem = pool.find((item) => item.answer === answer);
-        reply += `${poolItem?.emoji} -> **${count}** times\n`;
+        const csgValue = poolItem.retire * count;
+        const pointsValue = poolItem.points * count;
+        reply += ` - ${poolItem?.emoji}- **${count}**   ->   ${csgValue}<:csg:1338159695227129956>\n`;
+        totalRetire += csgValue;
+        totalPoints += pointsValue;
       }
     }
-
+     reply += `\nCSGs Spent = \`${iterations * 100}\`\nPoints Earned = \`${totalPoints}\`\nRetirement Value = \`${totalRetire}\``
     return interaction.reply(reply);
   },
 };
--- src/commands/emote.js
+++ src/commands/emote.js
@@ -1,11 +1,23 @@
 const { SlashCommandBuilder, MessageFlags } = require('discord.js');
-const emoteList = require("../utils/emoteList.js")
+const { emotePeepo, emoteCat, emotePanda, emoteOthers} = require("../utils/emoteList.js");
 
 module.exports = {
-    data: new SlashCommandBuilder()
+  data: new SlashCommandBuilder()
     .setName(`emote`)
     .setDescription(`free nitro experience`)
-    .addStringOption(option => 
+    .addStringOption((option) =>
+      option
+        .setName(`group`)
+        .setDescription(`Select a group of emoji to choose from`)
+        .setRequired(true)
+        .addChoices(
+          { name: `Peepos`, value: `peepo` },
+          { name: `Pandas`, value: `panda` },
+          { name: `Cats`, value: `cat` },
+          { name: `Others`, value: `others` }
+        )
+    )
+    .addStringOption((option) =>
       option
         .setName(`name`)
         .setDescription(`Emoji name here`)
@@ -13,30 +25,61 @@ module.exports = {
         .setRequired(true)
     ),
 
-    async execute(interaction) {
+  async execute(interaction) {
+    const emotName = interaction.options.getString("name");
+    const emoteList = [
+      ...emotePeepo,
+      ...emoteCat,
+      ...emotePanda,
+      ...emoteOthers,
+    ];
+    const emote = emoteList.find((e) => e.name === emotName);
 
-      const emotname = interaction.options.getString("name");
-      const emote = emoteList.find((e) => e.name === emotname);
+    if (emote) {
+      interaction.reply(emote.emote);
+    } else {
+      interaction.reply({
+        content: `No emoji found for **${emotName}**`,
+        flags: MessageFlags.Ephemeral,
+      });
+    }
+  },
 
-      if (emote) {
-        interaction.reply(emote.emote);
-      } else {
-        interaction.reply({
-          content: `No emoji found for **${emotname}**`,
-          flags: MessageFlags.Ephemeral,
-        });
-      }
-    },
+  async autocomplete(interaction) {
+    const groupName = interaction.options.getString("group");
+    const focusedValue = interaction.options.getFocused();
 
-    async autocomplete(interaction) {
-      const focusedValue = interaction.options.getFocused();
-      
-      const filtered = emoteList
-        .filter(emote => emote.name.toLowerCase().includes(focusedValue.toLowerCase()))
+    let filtered;
+    if (groupName === `peepo`) {
+      filtered = emotePeepo
+        .filter((emote) =>
+          emote.name.toLowerCase().includes(focusedValue.toLowerCase())
+        )
         .slice(0, 25)
-        .map(emote => ({ name: emote.name, value: emote.name }));
-  
-      interaction.respond(filtered);
+        .map((emote) => ({ name: emote.name, value: emote.name }));
+    } else if (groupName === `panda`) {
+      filtered = emotePanda
+        .filter((emote) =>
+          emote.name.toLowerCase().includes(focusedValue.toLowerCase())
+        )
+        .slice(0, 25)
+        .map((emote) => ({ name: emote.name, value: emote.name }));
+    } else if (groupName === `cat`) {
+      filtered = emoteCat
+        .filter((emote) =>
+          emote.name.toLowerCase().includes(focusedValue.toLowerCase())
+        )
+        .slice(0, 25)
+        .map((emote) => ({ name: emote.name, value: emote.name }));
+    } else if (groupName === `others`) {
+      filtered = emoteOthers
+        .filter((emote) =>
+          emote.name.toLowerCase().includes(focusedValue.toLowerCase())
+        )
+        .slice(0, 25)
+        .map((emote) => ({ name: emote.name, value: emote.name }));
     }
 
-}
+    interaction.respond(filtered);
+  },
+};
